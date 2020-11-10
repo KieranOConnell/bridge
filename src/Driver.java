444388c753ae80b1692333ace414ca87b103557e
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2020-11-10 17:00:35 +0000

    Added simple Driver class to show program in action

---
+++ src/Driver.java
@@ -0,0 +1,18 @@
+public class Driver {
+    public static void main(String[] args) {
+        Employee teacher = new Teacher("John", "Terry", Gender.MALE, 35);
+        teacher.work();
+
+        Employee developer = new Developer("Sarah", "Moriarty", Gender.FEMALE, 21);
+        developer.work();
+
+        Employee janitor = new Janitor("Harry", "Morgan", Gender.MALE, 52);
+        janitor.work();
+
+        Employee doctor = new Doctor("Chris", "Collins", Gender.MALE, 47);
+        doctor.work();
+
+        Employee accountant = new Accountant("Beatrice", "Beetroot", Gender.FEMALE, 68);
+        accountant.work();
+    }
+}
